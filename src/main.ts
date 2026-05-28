35009d427ee56187f84c2aeb10a10eca447954f9
ajay.patel <ajay.patel@appinventiv.com>
2026-05-28 17:20:19 +0530

    updating port

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,6 @@ async function bootstrap() {
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  await app.listen(3002);
+  await app.listen(3000);
 }
 bootstrap();
